f90c47b543424c17b992afc12ec5c0fc4dc01d0d
tienanh147 <tienanh1472000@gmail.com>
2023-01-14 21:32:00 +0700

    add charityfunding contract, test script and fix deploy script

---
+++ contracts/CharityFunding.sol
@@ -0,0 +1,101 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "./Project.sol";
+
+contract CharityFunding {
+    // [X] Anyone can start a funding project .
+    // [X] Get All project list
+    // [X]  contribute amount
+
+    event ProjectStarted(
+        address projectContractAddress,
+        address creator,
+        uint256 minContribution,
+        uint256 projectDeadline,
+        uint256 goalAmount,
+        uint256 currentAmount,
+        uint256 noOfContributors,
+        string title,
+        string desc,
+        uint256 currentState
+    );
+
+    event ContributionReceived(
+        address projectAddress,
+        uint256 contributedAmount,
+        address indexed contributor
+    );
+
+    Project[] private projects;
+
+    // @dev Anyone can start a fund rising
+    // @return null
+
+    function createProject(
+        uint256 minimumContribution,
+        uint256 deadline,
+        uint256 targetContribution,
+        string memory projectTitle,
+        string memory projectDesc
+    ) public {
+        deadline = deadline;
+
+        Project newProject = new Project(
+            msg.sender,
+            minimumContribution,
+            deadline,
+            targetContribution,
+            projectTitle,
+            projectDesc
+        );
+        projects.push(newProject);
+
+        emit ProjectStarted(
+            address(newProject),
+            msg.sender,
+            minimumContribution,
+            deadline,
+            targetContribution,
+            0,
+            0,
+            projectTitle,
+            projectDesc,
+            0
+        );
+    }
+
+    // @dev Get projects list
+    // @return array
+
+    function returnAllProjects() external view returns (Project[] memory) {
+        return projects;
+    }
+
+    // function getProjectAddressFilter(address _creator) external view returns (address[] memory) {
+    //     address[] memory projectAddressList;
+    //     for(uint256 i = 0; i < projects.length; i ++) {
+    //         if (projects[i].creator.address == _creator) projectAddressList[i] = address(projects[i]);
+    //     }
+    //     return projectAddressList;
+    // }
+    
+    // @dev User can contribute
+    // @return null
+
+    function contribute(address _projectAddress) public payable {
+        uint256 minContributionAmount = Project(_projectAddress)
+            .minimumContribution();
+        Project.State projectState = Project(_projectAddress).state();
+        require(projectState == Project.State.Fundraising, "Invalid state");
+        require(
+            msg.value >= minContributionAmount,
+            "Contribution amount is too low !"
+        );
+        // Call function
+        Project(_projectAddress).contribute{value: msg.value}(msg.sender);
+        // Trigger event
+        emit ContributionReceived(_projectAddress, msg.value, msg.sender);
+    }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,12 +3,12 @@ const hre = require("hardhat");
 async function main() {
 
   // We get the contract to deploy
-  const Crowdfunding = await hre.ethers.getContractFactory("Crowdfunding");
-  const crowdfunding = await Crowdfunding.deploy();
+  const CharityFunding = await hre.ethers.getContractFactory("CharityFunding");
+  const charityfunding = await CharityFunding.deploy();
 
-  await crowdfunding.deployed();
+  await charityfunding.deployed();
 
-  console.log("Crowdfunding deployed to:", crowdfunding.address);
+  console.log("CharityFunding deployed to:", charityfunding.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
